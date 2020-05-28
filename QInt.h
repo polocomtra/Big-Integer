884bef3f2881c89cce7a35b817e7e7fa782150e5
Sy-Pham <54675399+Sy-Pham@users.noreply.github.com>
2020-05-28 17:02:48 +0700

    update QInt.h
    convert QInt

--- QInt.h
+++ QInt.h
@@ -10,10 +10,26 @@ class QInt
 {
 private:
 	char bitArr[SIZE];
+	
+	//helper function
+	int handleBit(char bit);
+	string div2(string number);
+	
+	void binToQInt(string input);
+	void decToQInt(string input);
+	void hexToQInt(string input);
+	
+	
+	//display array of bits to screen
+	void displayQInt();
 
 public:
 	QInt();
+	QInt(string input, int coefficient);
 	~QInt();
+	
+	//display array of bits to screen
+	void displayQInt();
 
 };
 
