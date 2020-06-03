9bf629bcb1639f3fb8ca679f63b4fffce751bc99
Tien Tai Nguyen <nguyentientai.3325@gmail.com>
2020-06-03 13:27:00 +0700

    Hi

--- Application.cpp
+++ Application.cpp
@@ -3,104 +3,133 @@
 #include <iostream>
 #include <sstream>
 
-Application::Application(){
-    inFile = fstream("input.txt", ios::in);
-    outFile = fstream("output.txt",ios::out);
+Application::Application(char* inFilename, char* outFilename)
+{
+	inFile = fstream(inFilename, ios::in);
+	outFile = fstream(outFilename, ios::out);
 }
 
-Application::~Application(){
-    inFile.close();
-    outFile.close();
+Application::Application() {
+	inFile = fstream("input.txt", ios::in);
+	outFile = fstream("output2.txt", ios::out);
 }
 
-string Application::readLine(){
-    string line = "";
-    getline(inFile, line);
-    return line;
+Application::~Application() {
+	inFile.close();
+	outFile.close();
 }
 
-void Application::writeLine(string line){
-    outFile << line << endl;
+string Application::readLine() {
+	string line = "";
+	getline(inFile, line);
+	return line;
 }
 
-void Application::solve(string req){
-    stringstream inStream(req);
-    string p1 = "";
-    getline(inStream, p1, ' ');
-    string p2 = "";
-    getline(inStream, p2, ' ');
-    
-    if (p2 == "2" | p2 == "10" | p2 == "16") {
-        //convert base p1 into base p2
-        string p3 = "";
-		
-        getline(inStream, p3, ' ');
-        p3.erase(p3.end() - 1);
-        QInt a(p3, p1);
-		
-        writeLine(a.toString(p2));
-    } else if (p2 == "~") {
-        // operator ~
-        writeLine("*****");
-    } else if (p2 == "^") {
-		//operator ^
-        writeLine("*****");
-    } else if (p2 == "ror") {
-        //operator ror
-        string p3 = "";
-        getline(inStream, p3, ' ');
-        p3.erase(p3.end() - 1);
-        QInt a(p3, p1);
-        a.ror();
-        writeLine(a.toString(p1));
-    } else if (p2 == "rol") {
-        //operator rol
-        string p3 = "";
-        getline(inStream, p3, ' ');
-        p3.erase(p3.end() - 1);
-        QInt a(p3, p1);
-        a.rol();
-        writeLine(a.toString(p1));
-    } else {
-        //operator + - * / >> <<
-        string p3 = "";
-        string p4 = "";
-        getline(inStream, p3, ' ');
-        getline(inStream, p4, ' ');
-        p4.erase(p4.end() - 1);
-        if (p3 == "<<") {
-            //
-            writeLine("*****");
-        } else if (p3 == ">>") {
-            //
-            writeLine("*****");
-        } else {
-            QInt a(p2,p1);
-            QInt b(p4,p1);
-            QInt result;
-            if (p3 == "+") result = a+b;
-            else if (p3 == "-") result = a-b;
-            else if (p3 == "*") result = a*b;
-            else if (p3 == "/") result = a/b;
-            
-            writeLine(result.toString(p1));
-
-        }
-    } 
+void Application::writeLine(string line) {
+	outFile << line << endl;
 }
 
-void Application::start(){
-	
-	if(inFile == NULL || outFile == NULL){
+void Application::solve(string req) {
+	if (req == "10 -6529624872154135214755416638567 >> 67") {
+		cout << "";
+	}
+	stringstream inStream(req);
+	string p1 = "";
+	getline(inStream, p1, ' ');
+	string p2 = "";
+	getline(inStream, p2, ' ');
+
+	if (p2 == "2" | p2 == "10" | p2 == "16") {
+		//convert base p1 into base p2
+		string p3 = "";
+
+		getline(inStream, p3, ' ');
+		//p3.erase(p3.end() - 1);
+		QInt a(p3, p1);
+
+		writeLine(a.toString(p2));
+	}
+	else if (p2 == "~") {
+		// operator ~
+		string p3 = "";
+		getline(inStream, p3, ' ');
+		//p3.erase(p3.end() - 1);
+		QInt a(p3, p1);
+
+		writeLine((~a).toString(p1));
+	}
+	else if (p2 == "ror") {
+		//operator ror
+		string p3 = "";
+		getline(inStream, p3, ' ');
+		//p3.erase(p3.end() - 1);
+		QInt a(p3, p1);
+		a.ror();
+		writeLine(a.toString(p1));
+	}
+	else if (p2 == "rol") {
+		//operator rol
+		string p3 = "";
+		getline(inStream, p3, ' ');
+		//p3.erase(p3.end() - 1);
+		QInt a(p3, p1);
+		a.rol();
+		writeLine(a.toString(p1));
+	}
+	else {
+		//operator + - * / >> <<
+		string p3 = "";
+		string p4 = "";
+		getline(inStream, p3, ' ');
+		getline(inStream, p4, ' ');
+		//p4.erase(p4.end() - 1);
+		if (p3 == "<<") {
+
+			QInt a(p2, p1);
+			a = a << stoi(p4);
+			writeLine(a.toString(p1));
+
+		}
+		else if (p3 == ">>") {
+			//
+			QInt a(p2, p1);
+			a = a >> stoi(p4);
+			writeLine(a.toString(p1));
+
+		}
+		else {
+			QInt a(p2, p1);
+			QInt b(p4, p1);
+			QInt result;
+			if (p3 == "+") result = a + b;
+			else if (p3 == "-") result = a - b;
+			else if (p3 == "*") result = a * b;
+			else if (p3 == "/") result = a / b;
+			else if (p3 == "^") result = a ^ b;
+			else if (p3 == "|") result = a | b;
+			else if (p3 == "&") result = a & b;
+
+			writeLine(result.toString(p1));
+
+		}
+	}
+
+
+}
+
+void Application::start() {
+
+	if (!inFile && !outFile) {
 		cout << "File not found" << endl;
 		return;
 	}
-	
-    string req = readLine();
-	
-    while (!req.empty()) {
-        solve(req);
-        req = readLine();
-    }
+
+	string req = readLine();
+
+	while (!req.empty()) {
+		solve(req);
+		cout << req << endl;
+		req = readLine();
+	}
 }
 
--- QInt.h
+++ QInt.h
@@ -9,38 +9,47 @@ using namespace std;
 class QInt
 {
 private:
-    char bitArr[SIZE];
-    char getBit(int pos);
-    void setBit(int pos, char val);
-    void fromBin(string str);
-    void fromDec(string str);
-    void fromHex(string str);
-    void div2(string& str);
+	char bitArr[SIZE];
+	char getBit(int pos);
+	void setBit(int pos, char val);
+	void fromBin(string str);
+	void fromDec(string str);
+	void fromHex(string str);
+	void div2(string& str);
 
-    string removeRedundant(string result);
-    string add(string a, string b);
+	string removeRedundant(string result);
+	string add(string a, string b);
 public:
 
-    QInt();
-    ~QInt();
-    QInt(string str, string base);
-    QInt(const QInt& ot);
-    QInt operator+(const QInt& ot);
-    QInt operator-(const QInt& ot);
-    QInt operator*(const QInt& ot);
-    QInt operator/(const QInt& ot);
-    QInt operator+();
-    QInt operator-();
-    QInt twoComplement();
-    QInt abs();
+	QInt();
+	~QInt();
+	QInt(string str, string base);
+	QInt(const QInt& ot);
+	QInt operator+(const QInt& ot);
+	QInt operator-(const QInt& ot);
+	QInt operator*(const QInt& ot);
+	QInt operator/(const QInt& ot);
+	QInt operator+();
+	QInt operator-();
+	QInt twoComplement();
+	QInt abs();
 
-    void ror();
-    void rol();
-    string toString();
+	//Operator &,|,^,~,>>,<<
 
-    string toDec();
-    string toHex();
-    string toBin();
-    string toString(string base);
+	QInt operator&(const QInt& ot) const;
+	QInt operator|(const QInt& ot) const;
+	QInt operator^(const QInt& ot) const;
+	QInt operator~() const;
+
+	QInt operator>>(int num) const;
+	QInt operator<<(int num) const;
+
+	void ror();
+	void rol();
+	string toString();
+
+	string toDec();
+	string toHex();
+	string toBin();
+	string toString(string base);
 };
-
